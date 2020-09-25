05fd79a07a303df1991e2bd5949090060286a1c7
Mike Pham <mikephse@gmail.com>
2020-09-25 15:00:39 -0400

    TypeScript commit

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Fucking World');
-  });
-
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
-
-  server.listen()
+System.register(["http"], function (exports_1, context_1) {
+    "use strict";
+    var http_1, hostname, port, server;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (http_1_1) {
+                http_1 = http_1_1;
+            }
+        ],
+        execute: function () {
+            hostname = '127.0.0.1';
+            port = 3000;
+            server = http_1.default.createServer((req, res) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Hello Fucking World From Mike');
+            });
+            server.listen(port, hostname, () => {
+                console.log(`Server running at http://${hostname}:${port}/`);
+            });
+            server.listen();
+        }
+    };
+});
